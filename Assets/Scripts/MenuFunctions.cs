83c2ab3606fd2b77871e5aaadacd87d52248e151
Adam <adamowskijad22@op.pl>
2018-04-12 19:16:51 +0200

    Zaznaczanie i inventory

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -7,22 +7,27 @@ public class MenuFunctions : MonoBehaviour {
 
     public Canvas menuCanvas;
     public Canvas addSoldierCanvas;
+    public Canvas inventoryCanvas;
     public GameObject army;
+    GameObject currentObject;
     Vector3 detinationPosition;
     RaycastHit hit;
     bool addMilitaryStatus = false;
 
     private void Start()
     {
+        inventoryCanvas = inventoryCanvas.GetComponent<Canvas>();
         menuCanvas = menuCanvas.GetComponent<Canvas>();
         Time.timeScale = 0;
         addSoldierCanvas = addSoldierCanvas.GetComponent<Canvas>();
         menuCanvas.enabled = true;
         addSoldierCanvas.enabled = false;
+        inventoryCanvas.enabled = false;
     }
 
     public void ButtonStart()
     {
+        inventoryCanvas.enabled = false;
         menuCanvas.enabled = false;
         addSoldierCanvas.enabled = false;
         Time.timeScale = 1;
@@ -30,19 +35,18 @@ public class MenuFunctions : MonoBehaviour {
 
     public void ChangeAddMilitaryStatus()
     {
+        currentObject = Instantiate(army, new Vector3(0,0,0), new Quaternion(0, transform.rotation.y, 0, transform.rotation.w)) as GameObject;
+
         addMilitaryStatus = true;
     }
     private void Update()
     {
         this.AddItemInMousePressPosition();
-
     }
 
     private void AddItemInMousePressPosition() {
         if (addMilitaryStatus)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
                 if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
                 {
                     if (hit.transform.name == transform.name)
@@ -51,16 +55,21 @@ public class MenuFunctions : MonoBehaviour {
                     }
                     detinationPosition = hit.point + new Vector3(0, 1, 0);
                 }
-                Instantiate(army, detinationPosition, new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
+            currentObject.transform.position = detinationPosition;
+                //Instantiate(army, detinationPosition, new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
+            if (Input.GetMouseButtonDown(0))
+            {
                 addMilitaryStatus = false;
                 addSoldierCanvas.enabled = false;
+                inventoryCanvas.enabled = false;
             }
         }
     }
 
     public void ReloadCanvas()
     {
-        addSoldierCanvas.enabled = true;
+        addSoldierCanvas.enabled = false;
+        inventoryCanvas.enabled = true;
         menuCanvas.enabled = false;
     }
 }
