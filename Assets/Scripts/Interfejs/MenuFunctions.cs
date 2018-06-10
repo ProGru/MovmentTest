a5e163f9789498b1818db61ab02a2977da641e69
angelaDer <angela.derewienczuk@smcebi.edu.pl>
2018-06-10 17:57:13 +0200

    not working button

--- Assets/Scripts/Interfejs/MenuFunctions.cs
+++ Assets/Scripts/Interfejs/MenuFunctions.cs
@@ -24,6 +24,37 @@ public class MenuFunctions : MonoBehaviour {
     private MainManager mainManager;
     private int createTypeOf;
     public Text textPopup;
+    /// <summary>
+    /// ////
+    /// </summary>
+
+    public Text numberArmy;
+    public Text numberArmy2;
+    public Text numberArmy3;
+    public Text numberArmy4;
+    public Text numberArmy5;
+    public Button armySlot;
+    public Button armySlot2;
+    public Button armySlot3;
+    public Button armySlot4;
+    public Button armySlot5;
+
+    public Text numberEconomy1;
+    public Text numberEconomy2;
+    public Text numberEconomy3;
+    public Text numberEconomy4;
+    public Text numberEconomy5;
+    public Text numberEconomy6;
+    public Button economySlot1;
+    public Button economySlot2;
+    public Button economySlot3;
+    public Button economySlot4;
+    public Button economySlot5;
+    public Button economySlot6;
+
+    public Button cancelArmy5;
+    public Button cancelArmy4;
+
 
     private void Start()
     {
@@ -41,6 +72,51 @@ public class MenuFunctions : MonoBehaviour {
         rekrutacjaCanvas.enabled = false;
         infoWindow.SetActive(false);
         GoldButton();
+        //////
+
+        numberArmy = numberArmy.GetComponent<Text>();
+        numberArmy2 = numberArmy2.GetComponent<Text>();
+        numberArmy3 = numberArmy3.GetComponent<Text>();
+        numberArmy4 = numberArmy4.GetComponent<Text>();
+        numberArmy5 = numberArmy5.GetComponent<Text>();
+
+        numberEconomy1 = numberEconomy1.GetComponent<Text>();
+        numberEconomy2 = numberEconomy2.GetComponent<Text>();
+        numberEconomy3 = numberEconomy3.GetComponent<Text>();
+        numberEconomy4 = numberEconomy4.GetComponent<Text>();
+        numberEconomy5 = numberEconomy5.GetComponent<Text>();
+        numberEconomy6 = numberEconomy6.GetComponent<Text>();
+
+        armySlot = armySlot.GetComponent<Button>();
+        armySlot.onClick.AddListener(TaskOnClick);
+        armySlot2 = armySlot2.GetComponent<Button>();
+        armySlot2.onClick.AddListener(TaskOnClick_2);
+        armySlot3 = armySlot3.GetComponent<Button>();
+        armySlot3.onClick.AddListener(TaskOnClick_3);
+        armySlot4 = armySlot4.GetComponent<Button>();
+        armySlot4.onClick.AddListener(TaskOnClick_4);
+        armySlot5 = armySlot5.GetComponent<Button>();
+        armySlot5.onClick.AddListener(TaskOnClick_5);
+
+        economySlot1 = economySlot1.GetComponent<Button>();
+        economySlot1.onClick.AddListener(TaskOnClick);
+        economySlot2 = economySlot2.GetComponent<Button>();
+        economySlot2.onClick.AddListener(TaskOnClick);
+        economySlot3 = economySlot3.GetComponent<Button>();
+        economySlot3.onClick.AddListener(TaskOnClick);
+        economySlot4 = economySlot4.GetComponent<Button>();
+        economySlot4.onClick.AddListener(TaskOnClick);
+        economySlot5 = economySlot5.GetComponent<Button>();
+        economySlot5.onClick.AddListener(TaskOnClick);
+        economySlot6 = economySlot6.GetComponent<Button>();
+        economySlot6.onClick.AddListener(TaskOnClick);
+
+        cancelArmy5 = cancelArmy5.GetComponent<Button>();
+        cancelArmy5.onClick.AddListener(CancelOnClick1);
+
+        cancelArmy4 = cancelArmy4.GetComponent<Button>();
+        cancelArmy4.onClick.AddListener(CancelOnClick2);
+       
     }
 
     public void PopupWindow(string text) {
@@ -175,4 +251,87 @@ public class MenuFunctions : MonoBehaviour {
         rekrutacjaCanvas.enabled = true;
         economyCanvas.enabled = false;
     }
+
+
+    //just trying
+
+    public void TaskOnClick()
+    {
+        numberArmy.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick_2()
+    {
+        numberArmy2.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick_3()
+    {
+        numberArmy3.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick_4()
+    {
+        numberArmy4.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick_5()
+    {
+        numberArmy5.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick1()
+    {
+        numberEconomy1.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick2()
+    {
+        numberEconomy2.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick3()
+    {
+        numberEconomy3.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick4()
+    {
+        numberEconomy4.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick5()
+    {
+        numberEconomy5.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void TaskOnClick6()
+    {
+        numberEconomy6.text = "1";
+        Debug.Log("What happened?");
+    }
+
+    public void CancelOnClick1()
+    {
+        numberArmy5.text = "0";
+        Debug.Log("Is it working?");
+    }
+
+    public void CancelOnClick2()
+    {
+        numberArmy4.text = "0";
+        Debug.Log("Is it working?");
+    }
+
+ 
 }
